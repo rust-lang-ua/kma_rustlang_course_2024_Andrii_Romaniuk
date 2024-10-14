dfb638da055fa3bc5792f1cfe25b763bedad31f5
Andrii Romaniuk <andrewrom321@gmail.com>
2024-10-14 13:08:04 +0300

    ref

--- rustlings/exercises/collections/hashmap2.rs
+++ rustlings/exercises/collections/hashmap2.rs
@@ -12,8 +12,6 @@
 // Execute the command `rustlings hint hashmap2` if you need
 // hints.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 #[derive(Hash, PartialEq, Eq)]
@@ -33,12 +31,16 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         Fruit::Lychee,
         Fruit::Pineapple,
     ];
-
+    
     for fruit in fruit_kinds {
-        // TODO: Put new fruits if not already present. Note that you
-        // are not allowed to put any type of fruit that's already
-        // present!
-    }
+        basket.entry(fruit).or_insert(3);
+    };
+
+    // for fruit in fruit_kinds {
+    //     if !basket.contains_key(&fruit) {
+    //         basket.entry(&fruit).insert(fruit, 2);
+    //     };
+    // };
 }
 
 #[cfg(test)]
--- rustlings/exercises/strings/strings1.rs
+++ rustlings/exercises/strings/strings1.rs
@@ -1,8 +1,6 @@
 // strings1.rs
 // Make me compile without changing the function signature!
-// Execute `rustlings hint strings1` for hints ;)
-
-// I AM NOT DONE
+// Execute `rustlings hint strings1` for hints ;)T DONE
 
 fn main() {
     let answer = current_favorite_color();
@@ -10,5 +8,5 @@ fn main() {
 }
 
 fn current_favorite_color() -> String {
-    "blue"
+    String::from("blue")
 }
